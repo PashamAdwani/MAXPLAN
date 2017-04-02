719416360fcca9c53c7abf204952457b5c3d55f9
PashamAdwani <pasham_adwani11@yahoo.co.in>
2017-04-02 19:22:34 -0400

    Update Maxplan.java

--- Maxplan.java
+++ Maxplan.java
@@ -27,38 +27,57 @@ public class Maxplan {
 
     int taskCount;
 
-    Task[] optimizedArrayofTasks;
-
-    Task[] unoptimizedArrayofTasks;
+    Task[] optimizedArrayofTasks; // To be completed tasks
+    
+    Task[] unoptimizedArrayofTasks; //will have all the Task : old, new, and completed 
 
     float efficiency;
-
+    
 
 
 //--------------------------------------------------------------
 
     /**
+     * Priority: 1 to 5
+     * 
+     * Task Type : 0 --> Work
+     *             1 --> School
+     *             2 --> Personal
+     *
+     * isTaskCompleted : 0 --> Not Completed
+     *                   1 --> Completed              
      *
      */
 
     public class Task
     {
-        int TaskID;   //used for identification as an input for array(optimized)
         char[] TaskName;
         int TaskType;
         Date TaskDeadline;
         Date TaskCompleted;
         int TaskDifficulty;
         char[] TaskDescription;
+        boolean isTaskCompleted; 
     }
 //----------------------------------------------------------------
 
+    /**
+    *
+    * GUI function to get user's Input
+    *
+    */
+    
     public void getUserInput(Task t)
     {
 
     }
 
 //----------------------------------------------------------------
+    
+    /**
+    *Function to initialize a new Task dynamically
+    */
+    
     public void initTask(int nthTask)
     {
         Task t = new Task();
@@ -68,6 +87,11 @@ public class Maxplan {
 
 //-----------------------------------------------------------------
 
+    /**
+    * MOST IMPORTANT
+    * Takes the INCOMPLETE task from un-Optimized array and optimize it for best results 
+    */
+    
     public void calculateOptimizedArray(Task[] unoptimizedArray)
     {
 
@@ -75,13 +99,13 @@ public class Maxplan {
 
 //----------------------------------------------------------------
 
-     public float calculateEfficiency(Task[] optimizedArray)
+     public float calculateEfficiency(Task[] unoptimizedArray)
      {
         int totalTasksCompletedOnTime = 0;
          float efficiency;
            for(int i=0;i<taskCount;i++)
            {
-              if(optimizedArray[i].TaskDeadline > = optimizedArray[i].TaskCompleted)
+              if(unoptimizedArray[i].TaskDeadline > = unoptimizedArray[i].TaskCompleted && unoptimizedArray[i].isTaskCompleted == 1)
               {
                   totalTasksCompletedOnTime = totalTasksCompletedOnTime + 1;
               }
@@ -97,7 +121,7 @@ public class Maxplan {
         int taskDeclared = 0; // keeps a track of  number of tasks declared by USER/CUSTOMER
         //Maxplan GUI
         taskCount = taskDeclared;
-        efficiency = calculateEfficiency(optimizedArrayofTasks);
+        efficiency = calculateEfficiency(unoptimizedArrayofTasks);
     }
 
 //------------------------------------------------------------
